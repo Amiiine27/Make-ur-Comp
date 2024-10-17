6705ccdb69883a1ada1ee29dd8fdaaf758da3515
Amiiine27 <amineakh213@outlook.fr>
2024-10-17 13:26:20 +0200

    ok but not a good space betweent teams

--- src/main/kotlin/makeurcomp/Main.kt
+++ src/main/kotlin/makeurcomp/Main.kt
@@ -7,12 +7,12 @@ fun main() {
         Joueur("Nabil", "Aouraghe", 30, 9.0),
         Joueur("Faycal", "Beddiaf", 20, 8.0),
 
-        Joueur("Mehdi", "Bouainane", 20, 8.0),
+        Joueur("Mehdi", "Bouainane", 20, 8.5),
         Joueur("Axel", "Gerke", 54, 4.0),
-        Joueur("Jordan", "Dutallis", 29, 6.75),
+        Joueur("Jordan", "Dutallis", 29, 6.5),
         Joueur("Rayan", "Acheraiou", 21, 7.0),
 
-        Joueur("Yann-Yves", "Laplume", 37, 6.5),
+        Joueur("Yann-Yves", "Laplume", 37, 6.75),
         Joueur("Ahmed", "Elmzouri", 40, 7.5),
         Joueur("Thomas", "David", 24, 7.0),
         Joueur("Mickael", "Ettedgui", 43, 6.75)
@@ -24,7 +24,7 @@ fun main() {
     tableau.equipes.forEachIndexed { index, equipe ->
         println("Equipe ${index +1} : ")
         equipe.joueurs.forEach { joueur ->
-            println("${joueur.prenom} \t | ${joueur.nom}")
+            println("${joueur.prenom} \t | ${joueur.nom} \t | ${joueur.niveau}")
         }
         println("Moyenne de nieveau : ${equipe.moyenneNiveau()}")
         println()
